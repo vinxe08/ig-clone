f3880e6f54c72e389538600934f494e43115ff0e
vinxe08 <me.vince.garcia@gmail.com>
2023-01-09 03:06:24 +0800

    Fixing OAuthCallback error

--- app/auth/signin/SignUpComponent.tsx
+++ app/auth/signin/SignUpComponent.tsx
@@ -9,13 +9,13 @@ type Props = {
 }
 
 function SignUpComponent({provider, icon}: Props) {
-  console.log("Provider: ",provider)
+
   return (
     <div>
       <div>
         <button 
           onClick={() => signIn(provider?.id , {
-          callbackUrl: process.env.VERCEL_URL || "http://localhost:3000" })}
+          callbackUrl: process.env.VERCEL_URL || "https://5th-build-clone.vercel.app/" })}
           className='text-blue-900 font-semibold flex items-center space-x-2'>
           <div className='h-10 w-10'>
             <Lottie animationData={icon} loop={true}/>
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,14 +13,14 @@ export const authOptions = {
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
     }),
     InstagramProvider({
-      clientId: process.env.INSTAGRAM_CLIENT_ID,
-      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
+      clientId: process.env.INSTAGRAM_CLIENT_ID!,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET!,
     })
     // ...add more providers here
   ],
   
   // adapter: MongoDBAdapter(clientPromise), // THIS IS THE ISSUE
-  secret: process.env.NEXTAUTH_SECRET, 
+  secret: process.env.NEXTAUTH_SECRET!, 
   // generate-secret.vercel.app
   pages:{
     signIn: '/auth/signin',
